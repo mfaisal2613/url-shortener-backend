2418530499e0f11314f7406d27052c42f352c35a
Mohammad Faisal <mfaisal_26@hotmail.com>
2025-03-11 16:47:38 -0600

    added timeout

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000, // Set this higher if needed
   })
   .then(() => console.log(`Connected to MongoDB (${process.env.NODE_ENV})`))
   .catch((err) => console.error("MongoDB connection error:", err));
